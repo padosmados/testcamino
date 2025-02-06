639b9535dd3667f9b29ccb7f9df64eec9b05d8df
padosmados <paddypage@hotmail.com>
2025-02-06 19:52:42 +0000

    countries.ts

--- utils/countries.ts
+++ utils/countries.ts
@@ -1,11 +1,16 @@
 import countries from 'world-countries';
 
-export const formattedCountries = countries.map((item) => ({
-  code: item.cca2,
-  name: item.name.common,
-  flag: item.flag,
-  location: item.latlng,
-  region: item.region,
-}));
+// Filter the countries to only include France, Spain, and Portugal
+export const formattedCountries = countries
+  .filter(item => ['France', 'Spain', 'Portugal'].includes(item.name.common)) // Filter by country names
+  .map((item) => ({
+    code: item.cca2, // Country code (ISO 3166-1 alpha-2)
+    name: item.name.common, // Country name
+    flag: item.flag, // Country flag
+    location: item.latlng, // Latitude and longitude
+    region: item.region, // Region (e.g., Europe)
+  }));
+
+// Function to find country by code (this will work for only France, Spain, and Portugal)
 export const findCountryByCode = (code: string) =>
   formattedCountries.find((item) => item.code === code);
